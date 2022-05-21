43b14a6feca84f514622d6aadc756d68c5245aae
Marcoant007 <marcoantnovo@gmail.com>
2022-05-21 20:17:10 -0300

    corrigindo erro ao importar carros

--- src/modules/cars/infra/typeorm/entities/CarImage.ts
+++ src/modules/cars/infra/typeorm/entities/CarImage.ts
@@ -1,5 +1,5 @@
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
-import { v4 as uuidV4} from "uuid";
+import {v4 as uuidV4} from "uuid";
 
 @Entity("cars_image")
 class CarImage {
@@ -16,9 +16,9 @@ class CarImage {
     @CreateDateColumn()
     created_at: Date;
 
-    constructor(){
-        if(!this.id){
-            this.id === uuidV4();
+    constructor() {
+        if (!this.id) {
+            this.id = uuidV4();
         }
     }
     
