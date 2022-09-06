2c982ae90ec8d48bf46f040fd43536062f38b299
Marcoant007 <marcoantnovo@gmail.com>
2022-09-05 22:49:21 -0300

    database "rentx_test" does not exist

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -1,17 +1,16 @@
 
-import { Connection } from 'typeorm';
 import request from "supertest";
 import createConnection from "../../../../shared/infra/database";
 import { app } from "../../../../shared/infra/http/app";
 import { hash } from "bcryptjs";
 import {v4 as uuid} from "uuid";
+import { Connection } from "typeorm/connection/Connection";
 
 let connection : Connection;
-describe("Create Category Controller ", async()=> {
-   
-
-    beforeEach(async () => {
+describe("Create Category Controller ", () => {
+    beforeAll(async () => {
         connection = await createConnection();
+        await connection.runMigrations();
         const id = uuid();
         const password = await hash("admin", 8);
 
@@ -20,20 +19,48 @@ describe("Create Category Controller ", async()=> {
             VALUES ('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXX-XXXX')
         `);
 
-    })
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
     
-    it("should be able to create a new category", async ()=> {
+    it("should be able to create a new category", async () => {
         const responseToken = await request(app).post("/sessions").send({
             email: "admin@rentx.com.br",
             password: "admin",
 
         })
+        const { token } = responseToken.body;
+
         console.log(responseToken.body)
         const response = await request(app).post("/categories").send({
             name: "Supertest cars",
             description: "Supertest category"
+        }).set({
+            Authorization: `Bearer ${token}`,
         });
 
         expect(response.status).toBe(201);
-    })
+    });
+
+    it("should not be able to create category with name exists", async () => {
+        const responseToken = await request(app).post("/sessions").send({
+            email: "admin@rentx.com.br",
+            password: "admin",
+
+        })
+        const { token } = responseToken.body;
+
+        console.log(responseToken.body)
+        const response = await request(app).post("/categories").send({
+            name: "Supertest cars",
+            description: "Supertest category"
+        }).set({
+            Authorization: `Bearer ${token}`,
+        });
+
+        expect(response.status).toBe(400);
+    });
 })
